e22392a180a07984e4abccc9fb1b4ec3b042dc79
jonathan <jonathan.kablan@sooyoos.com>
2020-04-13 02:25:17 +0200

    template 5 update

--- app/theme_5.dart
+++ app/theme_5.dart
@@ -21,30 +21,31 @@ class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
-  final String background = "https://lh3.googleusercontent.com/proxy/LgWjzfHf6yiWdx51Eh9Qh8eqJ25qX5j8zVk93nNIT6sROgUXmu09p4u-MPA55SxnUi3_q8GI3RFFBQQTfqIdrDHp-RsEvHNakr-2l-YTSWkFoW8a_4iPjXZ7pTPMDBUx";
-  final String logo = "https://www.reviews.org/au/app/uploads/2019/11/disney-plus-logo.png";
+  final String background = "assets/img/background.png";
+  final String logo = "assets/img/logo.png";
   
-  final List<String> homeImages = [ "https://10wallpaper.com/wallpaper/1366x768/1911/The_Mandalorian_2019_Game_HD_Poster_1366x768.jpg",
-    "https://cdn.wallpapersafari.com/76/45/3YpNqe.jpg"
+  final List<String> homeImages = [ 
+    "assets/img/affiche_mandalorian.png",
+    "assets/img/affiche_starwars.png"
   ];
   
   final List<String> recommandedForYou = [
-    "https://fr.web.img2.acsta.net/pictures/16/09/09/08/57/518191.jpg",
-    "https://www.fanactu.com/medias/une-nouvelle-serie-d-affiches-personnages-pour-avengers-endgame_11434/avengers-endgame---captain-marvel-desktop-194109.jpg",
-    "https://www.dhresource.com/600x600/f2/albu/g7/M00/C8/C3/rBVaSVs3GuiAYtOPAAc1KmETVNg738.jpg"
+    "assets/img/doctor-strainge.png",
+    "assets/img/avengers-endgame.png",
+    "assets/img/venom.png"
   ];
   
   final List<String> tendances = [
-    "https://marvelll.fr/wp-content/uploads/2019/10/the-mandalorian-serie-disney-poster-personnage.jpg",
-    "https://www.ecranlarge.com/uploads/image/001/022/les-simpson-affiche-1022674.jpg",
-    "https://lh3.googleusercontent.com/proxy/3YXp-JlGWqygnGS3-wi4DbTNWguLTQBgI2MHyuVxY3LhSR8k03EquEvGyrjvKy6zbyM_dK9YhyfDzSgYhi388jyCxawSvogbHenkDJe-l-a7A03BCCjtV8kGEHJ6Ww",
+    "assets/img/mandalorian.png",
+    "assets/img/simpson.png",
+    "assets/img/lmulan.png",
   ];
   
-  final String disney = "https://pngimg.com/uploads/walt_disney/walt_disney_PNG38.png";
-  final String pixar = "https://www.prophet.com/relevantbrands-2016/wp-content/uploads/2016/10/pixar-logo-white.png";
-  final String marvel = "https://www.stickpng.com/assets/images/585f9333cb11b227491c3581.png";
-  final String starwars = "https://i.pinimg.com/originals/95/e4/fd/95e4fd78946f22995eedd9a66e9b794d.png";
-  final String nationalGeo = "https://pluspng.com/img-png/national-geographic-logo-png-logo-natgeo-png-pluspng-com-logo-national-geographic-png-1000.png";
+  final String disney = "assets/img/logo_disney.png";
+  final String pixar = "assets/img/logo_pixar.png";
+  final String marvel = "assets/img/logo_marvel.png";
+  final String starwars = "assets/img/logo_starwars.png";
+  final String nationalGeo = "assets/img/logo_national-geographic.png";
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -66,10 +67,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 width: double.infinity,
               ),
               Center(
-                child: new Image.network(
+                child: Image.asset(
                   widget.background,
                   width: size.width,
-                  height: 700,
+                  height: 760,
                   fit: BoxFit.cover,
                 ),
               ),
@@ -83,7 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         Padding(
                           padding: EdgeInsets.all(5.0),
                           child: Center(
-                            child: new Image.network(
+                            child: new Image.asset(
                               widget.logo,
                               width: 90.0,
                               height: 60.0,
@@ -94,7 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         Padding(
                           padding: const EdgeInsets.only(left: 10.0, top: 5.0, right: 10.0),
                           child: Container(
-                            height: 180,
+                            height: 210,
                             child: ListView.builder(
                               shrinkWrap: true,
                               itemCount: widget.homeImages.length,
@@ -103,6 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                 return Padding(
                                   padding: const EdgeInsets.only(right: 8.0),
                                   child: Container(
+                                    height: 200,
                                     child: Column(
                                       crossAxisAlignment: CrossAxisAlignment.start,
                                       children: <Widget>[
@@ -110,7 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                           children: <Widget>[
                                             ClipRRect(
                                               borderRadius: BorderRadius.circular(8.0),
-                                              child: Image.network(
+                                              child: Image.asset(
                                                 widget.homeImages[index],
                                                 width: size.width - 50,
                                                 height: size.height - 400,
@@ -132,191 +134,11 @@ class _MyHomePageState extends State<MyHomePage> {
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceAround,
                             children: <Widget>[
-                              Container(
-                                width: 50.0,
-                                height: 50.0,
-                                decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                    begin: Alignment.topCenter,
-                                    end: Alignment.bottomCenter,
-                                    stops: [
-                                      0.1,
-                                      0.3,
-                                      0.6,
-                                      0.9
-                                    ],
-                                    colors: [
-                                      Colors.blue[900],
-                                      Colors.blue[900],
-                                      Colors.blue[800],
-                                      Colors.blue[900]
-                                    ]),
-                                  shape: BoxShape.rectangle,
-                                  border: Border.all(color: Colors.white,width: 1),
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(5.0),
-                                    topRight: Radius.circular(5.0),
-                                    bottomLeft: Radius.circular(5.0),
-                                    bottomRight: Radius.circular(5.0)
-                                  ) 
-                                ),
-                                child: Center(
-                                  child: Image.network(
-                                    widget.disney,
-                                    width: 40,
-                                    height: 40,
-                                    fit: BoxFit.contain,
-                                  ),
-                                )
-                              ),
-                              Container(
-                                width: 50.0,
-                                height: 50.0,
-                                decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                    begin: Alignment.topCenter,
-                                    end: Alignment.bottomCenter,
-                                    stops: [
-                                      0.1,
-                                      0.3,
-                                      0.6,
-                                      0.9
-                                    ],
-                                    colors: [
-                                      Colors.blue[900],
-                                      Colors.blue[900],
-                                      Colors.blue[800],
-                                      Colors.blue[900]
-                                    ]),
-                                  shape: BoxShape.rectangle,
-                                  border: Border.all(color: Colors.white,width: 1),
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(5.0),
-                                    topRight: Radius.circular(5.0),
-                                    bottomLeft: Radius.circular(5.0),
-                                    bottomRight: Radius.circular(5.0)
-                                  ) 
-                                ),
-                                child: Center(
-                                  child: Image.network(
-                                    widget.pixar,
-                                    width: 70,
-                                    height: 80,
-                                    fit: BoxFit.fill,
-                                  ),
-                                )
-                              ),
-                              Container(
-                                width: 50.0,
-                                height: 50.0,
-                                decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                    begin: Alignment.topCenter,
-                                    end: Alignment.bottomCenter,
-                                    stops: [
-                                      0.1,
-                                      0.3,
-                                      0.6,
-                                      0.9
-                                    ],
-                                    colors: [
-                                      Colors.blue[900],
-                                      Colors.blue[900],
-                                      Colors.blue[800],
-                                      Colors.blue[900]
-                                    ]),
-                                  shape: BoxShape.rectangle,
-                                  border: Border.all(color: Colors.white,width: 1),
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(5.0),
-                                    topRight: Radius.circular(5.0),
-                                    bottomLeft: Radius.circular(5.0),
-                                    bottomRight: Radius.circular(5.0)
-                                  ) 
-                                ),
-                                child: Center(
-                                  child: Image.network(
-                                    widget.marvel,
-                                    width: 40,
-                                    height: 40,
-                                    fit: BoxFit.contain,
-                                  ),
-                                )
-                              ),
-                              Container(
-                                width: 50.0,
-                                height: 50.0,
-                                decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                    begin: Alignment.topCenter,
-                                    end: Alignment.bottomCenter,
-                                    stops: [
-                                      0.1,
-                                      0.3,
-                                      0.6,
-                                      0.9
-                                    ],
-                                    colors: [
-                                      Colors.blue[900],
-                                      Colors.blue[900],
-                                      Colors.blue[800],
-                                      Colors.blue[900]
-                                    ]),
-                                  shape: BoxShape.rectangle,
-                                  border: Border.all(color: Colors.white,width: 1),
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(5.0),
-                                    topRight: Radius.circular(5.0),
-                                    bottomLeft: Radius.circular(5.0),
-                                    bottomRight: Radius.circular(5.0)
-                                  ) 
-                                ),
-                                child: Center(
-                                  child: Image.network(
-                                    widget.starwars,
-                                    width: 40,
-                                    height: 40,
-                                    fit: BoxFit.contain,
-                                  ),
-                                )
-                              ),
-                              Container(
-                                width: 50.0,
-                                height: 50.0,
-                                decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                    begin: Alignment.topCenter,
-                                    end: Alignment.bottomCenter,
-                                    stops: [
-                                      0.1,
-                                      0.3,
-                                      0.6,
-                                      0.9
-                                    ],
-                                    colors: [
-                                      Colors.blue[900],
-                                      Colors.blue[900],
-                                      Colors.blue[800],
-                                      Colors.blue[900]
-                                    ]),
-                                  shape: BoxShape.rectangle,
-                                  border: Border.all(color: Colors.white,width: 1),
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(5.0),
-                                    topRight: Radius.circular(5.0),
-                                    bottomLeft: Radius.circular(5.0),
-                                    bottomRight: Radius.circular(5.0)
-                                  ) 
-                                ),
-                                child: Center(
-                                  child: Image.network(
-                                    widget.nationalGeo,
-                                    width: 40,
-                                    height: 40,
-                                    fit: BoxFit.contain,
-                                  ),
-                                )
-                              ),
+                              _itemContainer(widget.disney),
+                              _itemContainer(widget.pixar),
+                              _itemContainer(widget.marvel),
+                              _itemContainer(widget.starwars),
+                              _itemContainer(widget.nationalGeo),
                             ]
                           )
                         ),
@@ -361,7 +183,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                               children: <Widget>[
                                                 ClipRRect(
                                                   borderRadius: BorderRadius.circular(3.0),
-                                                  child: Image.network(
+                                                  child: Image.asset(
                                                     widget.recommandedForYou[index],
                                                     height: 140,
                                                     width: 100,
@@ -421,7 +243,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                               children: <Widget>[
                                                 ClipRRect(
                                                   borderRadius: BorderRadius.circular(3.0),
-                                                  child: Image.network(
+                                                  child: Image.asset(
                                                     widget.tendances[index],
                                                     height: 140,
                                                     width: 100,
@@ -480,6 +302,46 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _itemContainer(String image){
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          stops: [
+            0.1,
+            0.3,
+            0.6,
+            0.9
+          ],
+          colors: [
+            Colors.blue[900],
+            Colors.blue[900],
+            Colors.blue[800],
+            Colors.blue[900]
+          ]),
+        shape: BoxShape.rectangle,
+        border: Border.all(color: Colors.white,width: 1),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(5.0),
+          topRight: Radius.circular(5.0),
+          bottomLeft: Radius.circular(5.0),
+          bottomRight: Radius.circular(5.0)
+        ) 
+      ),
+      child: Center(
+        child: Image.asset(
+          image,
+          width: 40,
+          height: 40,
+          fit: BoxFit.contain,
+        ),
+      )
     );
   }
 }
